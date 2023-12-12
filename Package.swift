33ef47f22287b411a7a5cf389aaeb44b6e8ce0e7
Mykhailo Bondarenko <mcmikius@icloud.com>
2023-12-12 14:16:23 +0200

    build: add platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "fs-app-health-checks",
+    platforms: [
+        .macOS(.v13),
+        .iOS(.v16),
+        .watchOS(.v9),
+        .tvOS(.v16),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
