1c5839da396e01bf77fe74bdd9f4b7919ceb2b34
Mykola Buhaiov <gulivero1773@icloud.com>
2024-01-31 10:37:13 +0200

    refactor: change license in all files and add getApplicationHealth

--- Sources/AppHealthChecks/HealthChecks.ApplicationStatus/AppHealthChecks.swift
+++ Sources/AppHealthChecks/HealthChecks.ApplicationStatus/AppHealthChecks.swift
@@ -1,10 +1,25 @@
+// FS Dependency Injection
+// Copyright (C) 2024  FREEDOM SPACE, LLC
+
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published
+//  by the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 //
 //  AppHealthChecks.swift
 //
 //
 //  Created by Mykola Buhaiov on 30.01.2024.
-//  Copyright © 2024 Freedom Space LLC
-//  All rights reserved: http://opensource.org/licenses/MIT
 //
 
 import Vapor
@@ -13,7 +28,42 @@ import Vapor
 public struct AppHealthChecks {
     private let logger = Logger(label: "AppHealthChecks")
 
+
+    /// Get app major version
+    /// - Parameter serverVersion: `String`
+    /// - Returns: `Int`
+    public func getPublicVersion(from version: String?) -> Int {
+        let components = version?.components(separatedBy: ".")
+        guard let version = Int(components?.first ?? "0") else {
+            logger.error("In version: \(String(describing: version)) not found first index")
+            return 0
+        }
+        return version
+    }
+    
+    /// Get application health
+    /// - Parameter appInformation: `AppInformationDTO`
+    /// - Returns: `HealthCheck`
+    public func getApplicationHealth(from appInformation: AppInformationDTO) -> HealthCheck {
+        let healthCheck = HealthCheck(
+            status: appInformation.status,
+            version: appInformation.version,
+            releaseId: appInformation.releaseId,
+            notes: appInformation.notes,
+            output: appInformation.output,
+            checks: appInformation.checks,
+            links: appInformation.links,
+            serviceId: appInformation.serviceId,
+            description: appInformation.description
+        )
+        return healthCheck
+    }
+}
+
+public struct AppInformationDTO: Content {
     public var status: HealthCheckStatus?
+
+    public var version: Int?
 
     public var releaseId: String?
 
@@ -28,33 +78,4 @@ public struct AppHealthChecks {
     public var serviceId: UUID?
 
     public var description: String?
-
-    /// Get app major version
-    /// - Parameter serverVersion: `String`
-    /// - Returns: `Int`
-    public func getPublicVersion(from version: String?) -> Int {
-        let components = version?.components(separatedBy: ".")
-        guard let version = Int(components?.first ?? "0") else {
-            logger.error("In version: \(String(describing: version)) not found first index")
-            return 0
-        }
-        return version
-    }
-    
-    /// Get application health check
-    /// - Returns: `HealthCheck`
-    public func getAppHealthCheck() -> HealthCheck {
-        let healthCheck = HealthCheck(
-            status: self.status,
-            version: self.getPublicVersion(from: releaseId),
-            releaseId: self.releaseId,
-            notes: self.notes,
-            output: self.output,
-            checks: self.checks,
-            links: self.links,
-            serviceId: self.serviceId,
-            description: self.description
-        )
-        return healthCheck
-    }
 }
--- Sources/AppHealthChecks/Response/MeasurementType.swift
+++ Sources/AppHealthChecks/Response/MeasurementType.swift
@@ -1,10 +1,25 @@
+// FS Dependency Injection
+// Copyright (C) 2024  FREEDOM SPACE, LLC
+
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published
+//  by the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 //
 //  MeasurementType.swift
 //
 //
 //  Created by Mykola Buhaiov on 29.01.2024.
-//  Copyright © 2024 Freedom Space LLC
-//  All rights reserved: http://opensource.org/licenses/MIT
 //
 
 import Vapor
