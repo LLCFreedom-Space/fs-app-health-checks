3ccb8b5d360d8a5cb25bb5b375088fb5077ea55e
Mykola Buhaiov <gulivero1773@icloud.com>
2024-04-25 15:07:47 +0300

    refactor: fix error and refactor connection

--- Sources/HealthChecks/PostgresHealthChecks/PostgresHealthChecks.swift
+++ Sources/HealthChecks/PostgresHealthChecks/PostgresHealthChecks.swift
@@ -49,7 +49,7 @@ public struct PostgresHealthChecks: PostgresHealthChecksProtocol {
         let result = HealthCheckItem(
             componentId: app.psqlId,
             componentType: .datastore,
-            observedValue: 1,
+            observedValue: connectionDescription.contains("active") ? 1 : 0,
             status: connectionDescription.contains("active") ? .pass : .fail,
             time: app.dateTimeISOFormat.string(from: Date()),
             output: !connectionDescription.contains("active") ? connectionDescription : nil,
