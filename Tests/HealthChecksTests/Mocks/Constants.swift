4fd059ef7fd605f6b115f7c7fdbf7063969e6e9f
Mykola Buhaiov <gulivero1773@icloud.com>
2026-04-03 10:42:40 +0300

    fix: fix public to constans

--- Tests/HealthChecksTests/Mocks/Constants.swift
+++ Tests/HealthChecksTests/Mocks/Constants.swift
@@ -27,12 +27,10 @@ import Vapor
 
 enum Constants {
     // swiftlint:disable numbers_smell
-    /// PostgreSQL version description
-    public static let psqlVersionDescription =
+    static let psqlVersionDescription =
         """
         PostgreSQL 14.10 on x86_64-pc-linux-musl, compiled by gcc (Alpine 13.2.1_git20231014) 13.2.1 20231014, 64-bit
         """
+    static let consulUrl = "http://127.0.0.1:8500"
     // swiftlint:enable numbers_smell
-    /// Default url for consul
-    public static let consulUrl = "http://127.0.0.1:8500"
 }
