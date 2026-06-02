2d869d791b7aeb30578459a5be406f3fe478aafe
Mykola Buhaiov <gulivero1773@icloud.com>
2026-06-02 23:49:45 +0300

    fix: add test

---
+++ Tests/HealthChecksTests/Application+ExtensionsTests.swift
@@ -0,0 +1,56 @@
+// FS App Health Checks
+// Copyright (C) 2024  FREEDOM SPACE, LLC
+
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published
+//  by the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+//
+//  Application+ExtensionsTests.swift
+//  fs-app-health-checks
+//
+//  Created by Mykola Buhaiov on 02.06.2026.
+//
+
+@testable import HealthChecksMocks
+@testable import HealthChecks
+import VaporTesting
+import Testing
+
+@Suite("Application+Extensions tests")
+struct ApplicationExtensionsTests {
+    private func withApp(_ test: (Application) async throws -> ()) async throws {
+        let app = try await Application.make(.testing)
+        defer {
+            Task {
+                try? await app.asyncShutdown()
+            }
+        }
+        try await test(app)
+    }
+    
+    private static func connectionString() -> String {
+        guard let host = ProcessInfo.processInfo.environment["MONGO_HOST"] else {
+            return ""
+        }
+        let db = ProcessInfo.processInfo.environment["MONGO_DB"] ?? "vapor_test"
+        return "mongodb://\(host):27017/\(db)"
+    }
+
+    @Test("Init lazy mongo connection", .enabled(if: ProcessInfo.processInfo.environment["MONGO_HOST"] != nil))
+    func initLazyMongoConnection() async throws {
+        try await withApp { app in
+            await #expect(throws: Never.self) { try await app.initializeLazyMongo(connectionString: Self.connectionString()) }
+        }
+    }
+}
